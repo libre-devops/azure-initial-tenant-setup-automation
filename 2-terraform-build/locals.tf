8f0f9ac6902b6ff526b41fa048d6f7d8bd722ee7
Craig Thacker <craigthackerx@gmail.com>
2023-09-04 02:13:08 +0100

    Update

--- 2-terraform-build/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  module_tags = {
-    LastUpdated = formatdate("DD-MM-YYYY:hh:mm", timestamp())
-    ManagedBy   = "Terraform"
-    Contact     = "help@libredevops.org"
-  }
-
-  tags = merge(local.module_tags, var.tags)
-}
